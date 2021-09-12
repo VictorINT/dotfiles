98a537c6f978347f34e06280419dc74629bc3629
theunoriginalguy <victor.s.florescu@gmail.com>
2021-09-12 19:53:37 +0300

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,13 @@
+set fish_greeting
+# colorscript random
+pfetch
+
+alias rmf="rm -rf"
+alias ll='exa -l'
+alias la='ls -A'
+alias l='exa -1'
+alias lt='exa -T'
+alias flex='clear && neofetch'
+
+alias root="sudo -i"
+
