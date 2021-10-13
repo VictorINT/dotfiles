9b8b35f5580878a0a48060e02b3191f530ea1b0b
CookieFlop <victor.s.florescu@gmail.com>
2021-10-13 23:18:58 +0300

    neofetch config added + some folders

---
+++ term/config.fish
@@ -0,0 +1,17 @@
+set fish_greeting
+# colorscript random
+# pfetch
+
+alias rmf="rm -rf"
+alias ll='exa -l'
+alias la='ls -A'
+alias l='exa -1'
+alias lt='exa -T'
+
+alias flex='clear && neofetch'
+alias smallflex='clear && pfetch'
+
+alias clock='tty-clock -sct -f "%a, %d %b %Y %T %z"'
+
+alias root="sudo -i"
+
